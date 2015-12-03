6ac4d3eca86e8ac51bc9a1e4e1b3984f35e07784
Dillon Mulcahy <Dillon Mulcahy>
2015-12-03 17:14:07 -0500

    im so goood at programming

---
+++ Addition.cpp
@@ -0,0 +1,21 @@
+/*
+ * Addition.cpp
+ *
+ *  Created on: Dec 3, 2015
+ *      Author: DillonMulcahy
+ */
+
+#include "Addition.h"
+
+namespace std {
+
+Addition::Addition() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Addition::~Addition() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ Addition.h
@@ -0,0 +1,21 @@
+/*
+ * Addition.h
+ *
+ *  Created on: Dec 3, 2015
+ *      Author: DillonMulcahy
+ */
+
+#ifndef ADDITION_H_
+#define ADDITION_H_
+
+namespace std {
+
+class Addition {
+public:
+	Addition();
+	virtual ~Addition();
+};
+
+} /* namespace std */
+
+#endif /* ADDITION_H_ */
